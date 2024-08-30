a9e0fcf06fc8061abc368dac0f7f74d37b2c80a4
sourav@synkrama.com <sourav@synkrama.com>
2024-08-30 11:34:54 +0530

    Made necessay changes in validator files

---
+++ garage-service/src/public/custom.error.ts
@@ -0,0 +1,20 @@
+class MechanicSignUpIdAlreadyExistsError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "Mechanic SignUp Id Already Exists Error";
+  }
+}
+
+class GarageInformationIdAlreadyExistsError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "Garage Information Id Already Exists Error";
+  }
+}
+
+class ServiceIdAlreadyExistsError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "Service Id Already Exists Error";
+  }
+}
